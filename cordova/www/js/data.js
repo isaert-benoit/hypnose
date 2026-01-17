200ba3ac8dc550d0cfee0b1504a41f07ee875c00
isaert.benoit <isaert.benoit@gmail.com>
2026-01-17 16:15:17 +0100

    Add mecanic

---
+++ cordova/www/js/data.js
@@ -0,0 +1,90 @@
+var datas = {
+    "fr" : {
+        "categories" : {
+            "1" : {
+                "name" : "Ma categorie 1",
+                "isFree" : "1",
+                "id": "1",
+                "description" : "Ma description 1 bla bla bla",
+                "sessions":{
+                    "1" : {
+                        "name" : "ma session 1",
+                        "isFree" : 1,
+                        "description" : "Description de la session 1"
+                    },
+                    "2" : {
+                        "name" : "ma session 2",
+                        "isFree" : 1,
+                        "description" : "Description de la session 2"
+                    },
+                    "3" : {
+                        "name" : "ma session 3",
+                        "isFree" : 1,
+                        "description" : "Description de la session 3"
+                    },
+                    "4" : {
+                        "name" : "ma session 4",
+                        "isFree" : 1,
+                        "description" : "Description de la session 4"
+                    }
+                }
+            },
+            "2" : {
+                "name" : "Ma categorie 2",
+                "isFree" : "1",
+                "id": "2",
+                "description" : "Ma description 2 bla bla bla",
+                "sessions":{
+                    "1" : {
+                        "name" : "ma session 5",
+                        "isFree" : 1,
+                        "description" : "Description de la session 5"
+                    },
+                    "2" : {
+                        "name" : "ma session 6",
+                        "isFree" : 1,
+                        "description" : "Description de la session 6"
+                    },
+                    "3" : {
+                        "name" : "ma session 7",
+                        "isFree" : 1,
+                        "description" : "Description de la session 7"
+                    },
+                    "4" : {
+                        "name" : "ma session 8",
+                        "isFree" : 1,
+                        "description" : "Description de la session 8"
+                    }
+                }
+            },
+            "3" : {
+                "name" : "Ma categorie 3",
+                "isFree" : "1",
+                "id": "3",
+                "description" : "Ma description 3 bla bla bla",
+                "sessions":{
+                    "1" : {
+                        "name" : "ma session 6",
+                        "isFree" : 1,
+                        "description" : "Session 1 categ 3"
+                    },
+                    "2" : {
+                        "name" : "ma session 7",
+                        "isFree" : 1,
+                        "description" : "Session 1 categ 3"
+                    },
+                    "3" : {
+                        "name" : "ma session 7",
+                        "isFree" : 1,
+                        "description" : "Session 1 categ 4"
+                    },
+                    "4" : {
+                        "name" : "ma session 8",
+                        "isFree" : 1,
+                        "description" : "Session 1 categ 5"
+                    }
+                }
+            }
+        }
+    }
+}
--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -25,205 +25,114 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
 
-    /*var my_media = new Media(getCordovaPath() + "1_1.ogg",
-        // success callback
-        function () { alert("playAudio():Audio Success"); },
-        // error callback
-        function (err) { console.log(err);alert("playAudio():Audio Error: " + err.code); }
-    );*/
-    var my_media ;
+    fillCatgories();
 
-    var counter = 0;
-    var timerDur ;
-    var mediaTimer;
-    var currentMediaSrc = "";
-
-   /* var timerDur = setInterval(function() {
-        counter = counter + 100;
-        if (counter > 2000) {
-            clearInterval(timerDur);
-        }
-        var dur = my_media.getDuration();
-        if (dur > 0) {
-            clearInterval(timerDur);
-            document.getElementById('audio_duration').innerHTML = (dur) + " sec";
-        }
-    }, 100);
-
-
-    var mediaTimer = setInterval(function () {
-        // get media position
-        //var dur = my_media.getDuration();
-        my_media.getCurrentPosition(
-            // success callback
-            function (position) {
-                if (position > -1) {
-                    console.log((position) + " position en sec");
-                    document.getElementById('position').innerHTML = (position) + " sec";
-                    
-                }
-            },
-            // error callback
-            function (e) {
-                console.log("Error getting pos=" + e);
-            }
+    $( "#categories .cta-category" ).on( "click", function() {
+        hideAndShow(
+            $( "#listing_categories"),
+            $( "#listing_sessions")
         );
-    }, 1000);*/
+        let clickedCategory = $(this).attr('data-category-id');
+        fillSessions(clickedCategory); 
+    } );
 
-   /* document.getElementById('play_1').onclick = function () {
-        console.log(my_media);
-        if(my_media != undefined && my_media.hasOwnProperty('id')){
-            console.log('stop by play : '+ my_media.id);
-          //  my_media.stop();
-        } else {
-            my_media = new Media(getCordovaPath() + "1_1.ogg",
-            // success callback
-            function () { alert("playAudio():Audio Success 1"); },
-            // error callback
-            function (err) { console.log(err);alert("playAudio():Audio Error: " + err.code); }
+    $( ".cta-return-categories" ).on( "click", function() {
+        hideAndShow(
+            $( "#listing_sessions"),
+            $( "#listing_categories")            
         );
-  
+    } );
+
+    $( ".cta-return-sessions" ).on( "click", function() {
+        hideAndShow(
+            $( "#player"),
+            $( "#listing_sessions")        
+        );
+    } );
+
+  };    
+
+ /** Fill Player on click cta */
+ function fillPlayer(categoryId, sessionId){
+
+    if(
+        categoryId != null
+        && categoryId != undefined
+        && sessionId != null
+        && sessionId != undefined
+    ){
+        let session = datas.fr.categories[categoryId].sessions[sessionId];
+        let sessionHtml = "<div>"+session.name+"</div>";
+        $('#player #details').html(sessionHtml) ;
+    }
+  }
+
+
+  /** Fill Session on click cta */
+  function fillSessions(categoryId){
+
+    if(categoryId != null && categoryId != undefined){
+
+        let dataCategory = datas.fr.categories[categoryId];
+        let sessions = dataCategory.sessions;
+        let productHtml = "";
+
+        for (let j = 0; j < Object.keys(sessions).length; j++) {
+            productHtml += "<div data-session-id='"+(j+1)+"' data-category-id='"+categoryId+"' class='cta-session'>"+sessions[j+1].name+"</div>";
         }
 
-        console.log(my_media.id);
-        my_media.play();
-    };*/
+        $('#listing_sessions #sessions').html(productHtml) ;
 
-    $( ".play" ).on( "click", function() {
+    }
 
-        let mediaSrc = $(this).data( "media_src" );
-
-        if(mediaSrc != currentMediaSrc){
-
-            if(my_media != undefined && my_media.hasOwnProperty('id')){
-                console.log('stop by play : '+ my_media.id);
-                my_media.stop();
-            }
-
-            my_media = new Media(getCordovaPath() + mediaSrc,
-                // success callback
-                function () { alert("playAudio():Audio Success 2"); },
-                // error callback
-                function (err) { console.log(err);alert("playAudio():Audio Error: " + err.code); }
-            );
-        }        
+    /** Add click event on session cta */
+    $( "#sessions .cta-session" ).on( "click", function() {
+        hideAndShow(
+            $( "#listing_sessions"),
+            $( "#player")            
+        );
+        let clickedCategory = $(this).attr('data-category-id');
+        let clickedSession = $(this).attr('data-session-id');
+        fillPlayer(
+            clickedCategory,
+            clickedSession
+        ); 
+    } );
 
 
-        console.log(my_media.id);
+  }
 
+  /** Fill Categories on load */
+  function fillCatgories(){
 
-        timerDur = setInterval(function() {
-            counter = counter + 100;
-            if (counter > 2000) {
-                clearInterval(timerDur);
-            }
-            var dur = my_media.getDuration();
-            if (dur > 0) {
-                clearInterval(timerDur);
-                document.getElementById('audio_duration').innerHTML = (dur) + " sec";
-            }
-        }, 100);
+    let categorieHtml = "";
 
-        mediaTimer = setInterval(function () {
-            // get media position
-            //var dur = my_media.getDuration();
-            my_media.getCurrentPosition(
-                // success callback
-                function (position) {
-                    if (position > -1) {
-                        console.log((position) + " position en sec");
-                        document.getElementById('position').innerHTML = (position) + " sec";
-                        
-                    }
-                },
-                // error callback
-                function (e) {
-                    console.log("Error getting pos=" + e);
-                }
-            );
-        }, 1000);
+    if(datas.fr.categories){
+        let categories = datas.fr.categories;
+        
+        for (let i = 0; i < Object.keys(categories).length; i++) {
+            //console.log(categories[i]);
+            categorieHtml += "<div data-category-id='"+(i+1)+"' class='cta-category'>"+categories[i+1].name+"</div>";
 
+            // product
+            // let sessions = categories[i+1].sessions;
+            // let productHtml = "";
 
-        currentMediaSrc = mediaSrc;
-        my_media.play();
-    });
+            // for (let j = 0; j < Object.keys(sessions).length; j++) {
+            //     productHtml += " --- " + sessions[j+1].name + "<br>";
+            // }
 
-   /* document.getElementById('play_2').onclick = function () {
+            // categorieHtml += productHtml ;
 
-        if(my_media != undefined && my_media.hasOwnProperty('id')){
-            console.log('stop by play : '+ my_media.id);
-            my_media.stop();
         }
 
+        $('#listing_categories #categories').html(categorieHtml) ;
 
-        my_media = new Media(getCordovaPath() + "1_2.mp3",
-            // success callback
-            function () { alert("playAudio():Audio Success 2"); },
-            // error callback
-            function (err) { console.log(err);alert("playAudio():Audio Error: " + err.code); }
-        );
-        console.log(my_media.id);
-        my_media.play();
-    };
-*/
-    document.getElementById('pause').onclick = function () {
-        alert('pause');
-        my_media.pause();
-    };
+    }
 
+  }
 
-    document.getElementById('stop').onclick = function () {
-        alert('stop');
-        console.log(my_media.id);
-        my_media.stop();
-    };
-
-
-    document.getElementById('ff').onclick = function () {
-        my_media.getCurrentPosition((pos)=>{
-            let dur = my_media.getDuration();
-            console.log('current position', pos);
-            console.log('duration', dur);
-            pos += 10;
-            if(pos < dur){
-                my_media.seekTo( pos * 1000 );
-            }
-        });
-    };
-
-    document.getElementById('rew').onclick = function () {
-        my_media.getCurrentPosition((pos)=>{
-            pos -= 10;
-            if( pos > 0){
-                my_media.seekTo( pos * 1000 );
-            }else{
-                my_media.seekTo(0);
-            }
-        });
-    };
-    
-
-
-}
-
-
-function getCordovaPath() {
-
-    var path = "";
-    if (device.platform == "Android") {
-       path = "file:///android_asset/www/";
-    } 
-    return path;
-}
-
-
-
-function playAudio(url) {
-    // Play the audio file at url
-   // alert(getCordovaPath() + url);
-    //my_media = 
-
-    // Play audio
-    my_media.play();
-
-}
+  function hideAndShow(hideElmt, showElmt){
+    hideElmt.hide();
+    showElmt.show();
+  }
