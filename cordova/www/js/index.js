e72761d0944aefec32c7917acf20bf6dab1e41af
isaert.benoit <isaert.benoit@gmail.com>
2026-01-21 21:55:58 +0100

    Refacto

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -1,138 +1,140 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+const App = {
+    currentCategory: null,
+    currentSession: null,
+    audioPlayer: new Audio(),
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    init: function() {
+        this.renderCategories();
+        this.bindEvents();
+        this.setupAudioListeners(); // Pour gérer la fin de piste par exemple
+    },
 
-
-
-function onDeviceReady() {
-
-    fillCatgories();
-
-    $( "#categories .cta-category" ).on( "click", function() {
-        hideAndShow(
-            $( "#listing_categories"),
-            $( "#listing_sessions")
-        );
-        let clickedCategory = $(this).attr('data-category-id');
-        fillSessions(clickedCategory); 
-    } );
-
-    $( ".cta-return-categories" ).on( "click", function() {
-        hideAndShow(
-            $( "#listing_sessions"),
-            $( "#listing_categories")            
-        );
-    } );
-
-    $( ".cta-return-sessions" ).on( "click", function() {
-        hideAndShow(
-            $( "#player"),
-            $( "#listing_sessions")        
-        );
-    } );
-
-  };    
-
- /** Fill Player on click cta */
- function fillPlayer(categoryId, sessionId){
-
-    if(
-        categoryId != null
-        && categoryId != undefined
-        && sessionId != null
-        && sessionId != undefined
-    ){
-        let session = datas.fr.categories[categoryId].sessions[sessionId];
-        let sessionHtml = "<div>"+session.name+"</div>";
-        $('#player #details').html(sessionHtml) ;
-    }
-  }
-
-
-  /** Fill Session on click cta */
-  function fillSessions(categoryId){
-
-    if(categoryId != null && categoryId != undefined){
-
-        let dataCategory = datas.fr.categories[categoryId];
-        let sessions = dataCategory.sessions;
-        let productHtml = "";
-
-        for (let j = 0; j < Object.keys(sessions).length; j++) {
-            productHtml += "<div data-session-id='"+(j+1)+"' data-category-id='"+categoryId+"' class='cta-session'>"+sessions[j+1].name+"</div>";
-        }
-
-        $('#listing_sessions #sessions').html(productHtml) ;
-
-    }
-
-    /** Add click event on session cta */
-    $( "#sessions .cta-session" ).on( "click", function() {
-        hideAndShow(
-            $( "#listing_sessions"),
-            $( "#player")            
-        );
-        let clickedCategory = $(this).attr('data-category-id');
-        let clickedSession = $(this).attr('data-session-id');
-        fillPlayer(
-            clickedCategory,
-            clickedSession
-        ); 
-    } );
-
-
-  }
-
-  /** Fill Categories on load */
-  function fillCatgories(){
-
-    let categorieHtml = "";
-
-    if(datas.fr.categories){
-        let categories = datas.fr.categories;
+    // 1. Rendu des catégories
+    renderCategories: function() {
+        const container = $('#categories');
+        container.empty();
         
-        for (let i = 0; i < Object.keys(categories).length; i++) {
-            //console.log(categories[i]);
-            categorieHtml += "<div data-category-id='"+(i+1)+"' class='cta-category'>"+categories[i+1].name+"</div>";
+        // On boucle sur l'objet 'datas' de ton fichier data.js
+        $.each(datas.fr.categories, (id, cat) => {
+            container.append(`
+                <div class="card category-item" data-id="${id}">
+                    <h3>${cat.name}</h3>
+                    <p>${cat.description}</p>
+                </div>
+            `);
+        });
+    },
 
-            // product
-            // let sessions = categories[i+1].sessions;
-            // let productHtml = "";
+    // 2. Navigation et Events
+    bindEvents: function() {
+        const self = this;
 
-            // for (let j = 0; j < Object.keys(sessions).length; j++) {
-            //     productHtml += " --- " + sessions[j+1].name + "<br>";
-            // }
+        // Clic sur une catégorie
+        $(document).on('click', '.category-item', function() {
+            const id = $(this).data('id');
+            self.showSessions(id);
+        });
 
-            // categorieHtml += productHtml ;
+        // Clic sur une session
+        $(document).on('click', '.session-item', function() {
+            const sessionId = $(this).data('id');
+            self.showPlayer(sessionId);
+        });
 
+        // Retours
+        $('.cta-return-categories').click(() => self.switchScreen('listing_categories'));
+        $('.cta-return-sessions').click(() => self.switchScreen('listing_sessions'));
+
+        // Player controls
+        $('.cta-play').click(() => self.playAudio());
+        $('.cta-pause').click(() => self.pauseAudio());
+    },
+
+    showSessions: function(catId) {
+        this.currentCategory = datas.fr.categories[catId];
+        const container = $('#sessions');
+        container.empty();
+
+        $.each(this.currentCategory.sessions, (id, session) => {
+            container.append(`
+                <div class="card session-item" data-id="${id}">
+                    <h4>${session.name}</h4>
+                    <p>${session.description}</p>
+                </div>
+            `);
+        });
+
+        this.switchScreen('listing_sessions');
+    },
+
+    showPlayer: function(sessionId) {
+        // On récupère la session visée
+        const targetSession = this.currentCategory.sessions[sessionId];
+
+        // LOGIQUE CRUCIALE : On vérifie si c'est une nouvelle session
+        if (this.currentSessionId !== sessionId) {
+            this.currentSession = targetSession;
+            this.currentSessionId = sessionId;
+            
+            // On ne change la source que si c'est une nouvelle piste
+            if(this.currentSession.src) {
+                this.audioPlayer.src = this.currentSession.src;
+                this.audioPlayer.load(); 
+            }
         }
 
-        $('#listing_categories #categories').html(categorieHtml) ;
+        // Mise à jour de l'UI (Détails de la session)
+        $('#details').html(`
+            <div class="player-view">
+                <h2>${this.currentSession.name}</h2>
+                <p>${this.currentSession.description}</p>
+            </div>
+        `);
 
+        // On ajuste l'état des boutons Play/Pause selon si l'audio tourne déjà
+        this.updatePlayerButtons();
+        this.switchScreen('player');
+    },
+
+    switchScreen: function(screenId) {
+        $('.screen').hide(); // On cache tout (ajoute class="screen" à tes divs parents)
+        $('#' + screenId).fadeIn();
+    },
+
+    updatePlayerButtons: function() {
+        if (!this.audioPlayer.paused) {
+            $('.cta-play').hide();
+            $('.cta-pause').show();
+        } else {
+            $('.cta-play').show();
+            $('.cta-pause').hide();
+        }
+    },
+
+    playAudio: function() {
+        this.audioPlayer.play();
+        this.updatePlayerButtons();
+    },
+
+    pauseAudio: function() {
+        this.audioPlayer.pause();
+        this.updatePlayerButtons();
+    },
+
+    switchScreen: function(screenId) {
+        $('.screen').hide();
+        $('#' + screenId).fadeIn();
+    },
+
+    setupAudioListeners: function() {
+        // Si la piste se finit, on remet le bouton Play
+        this.audioPlayer.onended = () => {
+            this.updatePlayerButtons();
+        };
     }
+};
 
-  }
-
-  function hideAndShow(hideElmt, showElmt){
-    hideElmt.hide();
-    showElmt.show();
-  }
+// Initialisation Cordova
+document.addEventListener('deviceready', () => {
+    App.init();
+}, false);
